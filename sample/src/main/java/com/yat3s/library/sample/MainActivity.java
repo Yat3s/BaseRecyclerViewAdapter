99599ae2ca92131f71dd46b0aa9fadfcebfe6d57
Yat3s <4645643@qq.com>
2016-06-14 16:32:35 +0800

    Modify as a library and created sample moudule

--- sample/src/main/java/com/yat3s/library/sample/MainActivity.java
+++ sample/src/main/java/com/yat3s/library/sample/MainActivity.java
@@ -1,4 +1,4 @@
-package com.yat3s.library.adapter;
+package com.yat3s.library.sample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,5 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
